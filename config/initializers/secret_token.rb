fcc2837c11d5cc535c4fbfb3c5e491cd67ec9e17
Andrey Krahmal <coolrighthere@yahoo.com>
2014-03-17 11:47:35 +0000

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scupr::Application.config.secret_key_base = '1e17b3a871618c745bfdc94616bc72e41d64fac4d5ffae4e7e9ca153ec779615be5eab76f9de102f924b0f096f238c7f39f418f860e7a9096b68b052578952bf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Scupr::Application.config.session_store :cookie_store, key: '_scupr_session'
