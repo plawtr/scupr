07e2908e5a69883dca0dd1b86512f68efc66eff1
Andrey Krahmal <coolrighthere@yahoo.com>
2014-03-27 18:31:02 +0000

    darn sorting is now sorting, we think.

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -3,7 +3,7 @@ class AdsController < ApplicationController
 	def index
 		if params[:coords]
 			@ads = Ad.all.select{|ad| ad.business.serves?(user_GPS)}
-  		render :json => {	:ads => @ads.order("updated_at DESC").as_json(user_GPS).sort_by{|ad| ad[:distance]} }
+  		render :json => {	:ads => @ads.sort_by{|ad| ad.updated_at}.reverse.as_json(user_GPS).sort_by{|ad| ad[:distance]} }
   	else
   		render :text => "Oops, it looks like we could not locate you! "
   	end
