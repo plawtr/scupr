9ee3afa8be44b8e8a0b89521015b6f62ecba3a57
Berta-G <berta.gutierrez@gmail.com>
2014-03-31 17:01:35 +0100

    sending updated ago

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,5 +1,7 @@
 class Ad < ActiveRecord::Base
 
+  include ActionView::Helpers::DateHelper
+
 	has_attached_file :image, :styles => { :large => "600x600", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates_presence_of :image
@@ -16,8 +18,12 @@ class Ad < ActiveRecord::Base
     image.url(:medium) # .split("?").first
   end
 
+  def updated_ago
+    time_ago_in_words(updated_at, include_seconds: true)
+  end
+
   def as_json(user_GPS)
-  	{id: id, bucket_image: bucket_image, image: full_image, caption: caption, business_name: business.name, business_GPS: {lat: business.lat, lng: business.lng}, distance: business.kms_to(user_GPS), business_phone: business.phone}
+  	{id: id, bucket_image: bucket_image, image: full_image, caption: caption, business_name: business.name, business_GPS: {lat: business.lat, lng: business.lng}, distance: business.kms_to(user_GPS), business_phone: business.phone, updated_ago: updated_ago}
   end
 
 end
