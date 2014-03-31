72be5fe1ff174ad769719a9cadd2ff237ffd7f8e
Berta-G <berta.gutierrez@gmail.com>
2014-03-31 18:32:25 +0100

    changed time ago format

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,6 +1,6 @@
 class Ad < ActiveRecord::Base
 
-  include ActionView::Helpers::DateHelper
+    
 
 	has_attached_file :image, :styles => { :large => "600x600", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
@@ -18,12 +18,24 @@ class Ad < ActiveRecord::Base
     image.url(:medium) # .split("?").first
   end
 
-  def updated_ago
-    time_ago_in_words(updated_at, include_seconds: true)
+  def time_ago
+    diff_seconds = (Time.now - updated_at).to_i
+    case diff_seconds
+      when 0..59
+        "#{diff_seconds}s"
+      when 60..3599
+        "#{diff_seconds/60}m"
+      when 3600 .. (3600*24-1)
+        "#{diff_seconds/3600}h"
+      when (3600*24) .. (3600*24*365)
+        "#{diff_seconds/(3600*24)}d"
+    else
+      start_time.strftime("%-d %b")
+    end
   end
 
   def as_json(user_GPS)
-  	{id: id, bucket_image: bucket_image, image: full_image, caption: caption, business_name: business.name, business_GPS: {lat: business.lat, lng: business.lng}, distance: business.kms_to(user_GPS), business_phone: business.phone, updated_ago: updated_ago}
+  	{id: id, bucket_image: bucket_image, image: full_image, caption: caption, business_name: business.name, business_GPS: {lat: business.lat, lng: business.lng}, distance: business.kms_to(user_GPS), business_phone: business.phone, updated_ago: time_ago}
   end
 
 end
